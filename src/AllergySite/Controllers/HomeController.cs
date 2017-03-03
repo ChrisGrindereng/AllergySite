533bc7e89ba1f4f729595c3a98f64916fdf827be
cog042 <cog042@gmail.com>
2017-03-02 22:12:42 -0600

    updated index and added main views

--- src/AllergySite/Controllers/HomeController.cs
+++ src/AllergySite/Controllers/HomeController.cs
@@ -27,6 +27,48 @@ namespace AllergySite.Controllers
             return View();
         }
 
+        public IActionResult Physicians()
+        {
+            ViewData["Message"] = "Physicians";
+
+            return View();
+        }
+
+        public IActionResult Offices()
+        {
+            ViewData["Message"] = "Offices";
+
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            ViewData["Message"] = "Conditions we Treat";
+
+            return View();
+        }
+
+        public IActionResult NewPatient()
+        {
+            ViewData["Message"] = "New to the Practice?";
+
+            return View();
+        }
+
+        public IActionResult PollenMap()
+        {
+            ViewData["Message"] = "Pollen Levels in Your Area.";
+
+            return View();
+        }
+
+        public IActionResult AppointmentForm()
+        {
+            ViewData["Message"] = "Schedule an Appointment!";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View();
