f7a25264b1be8ae9e9e92afad2d5081ea4a63943
cog042 <cog042@gmail.com>
2017-03-06 21:59:33 -0600

    added utils for JSON API and offices maps

---
+++ src/AllergySite/Utils/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace AllergySite.Utils
+{
+    internal static class API
+    {
+        public static async Task<string> GetJSON(string url) =>
+            await new HttpClient().GetStringAsync(url);
+
+        public static async Task<T> GetData<T>(string url) =>
+            JsonConvert.DeserializeObject<T>(await GetJSON(url));
+
+        public static string ToJSON(Object o) => JsonConvert.SerializeObject(o);
+    }
+}
