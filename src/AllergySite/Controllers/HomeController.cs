224c20c558bec47ba85f5f05a50664961d45f848
cog042 <cog042@gmail.com>
2017-03-09 22:15:47 -0600

    load after git crash

---
+++ src/AllergySite/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllergySite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Physicians()
+        {
+            ViewData["Message"] = "Physicians";
+
+            return View();
+        }
+
+        public IActionResult Offices()
+        {
+            ViewData["Message"] = "Offices";
+
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            ViewData["Message"] = "Conditions we Treat";
+
+            return View();
+        }
+
+        public IActionResult NewPatient()
+        {
+            ViewData["Message"] = "New to the Practice?";
+
+            return View();
+        }
+
+        public IActionResult PollenMap()
+        {
+            ViewData["Message"] = "Pollen Levels in Your Area.";
+
+            return View();
+        }
+
+        public IActionResult AppointmentForm()
+        {
+            ViewData["Message"] = "Schedule an Appointment!";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/AllergySite/Utils/API.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace AllergySite.Utils
+{
+    internal static class Utils
+    {
+
+        public static string ToJSON(this Object me) => JsonConvert.SerializeObject(me);
+
+        public static void Log(this Object me) => Console.WriteLine(me.ToJSON());
+
+        public static Object ToErrorObject(this ModelStateDictionary d)
+        {
+            return new
+            {
+                Errors = d.Values.Aggregate(
+                    new List<string>(),
+                    (acc, o) => {
+                        foreach (var error in o.Errors)
+                            acc.Add(error.ErrorMessage);
+
+                        return acc;
+                    }
+                )
+            };
+        }
+    }
+}
